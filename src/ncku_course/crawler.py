4085fcf64d3dd9d5496842a4544bcf02b2899aa9
黃和毅 <heyihuang826@gmail.com>
2024-12-17 21:21:56 +0800

    feat: add year and semeter with start and end params

--- src/ncku_course/crawler.py
+++ src/ncku_course/crawler.py
@@ -21,6 +21,10 @@ def get_url(cosname, teaname, wk = '', dept_no = '', degree = '', cl = '') -> st
         'dept_no': f'{dept_no}', 
         'degree': f'{degree}', 
         'cl': f'{cl}'
+        'syear_b': '113', 
+        'syear_e': '113', 
+        'sem_b': '2', 
+        'sem_e': '2'
         }
     headers = {
         'Accept-Encoding': 'gzip, deflate, br, zstd', 
