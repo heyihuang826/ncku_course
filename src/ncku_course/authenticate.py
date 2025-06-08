d427dab1d0fa608cf17cef29656baac108805e9d
heyihuang826 <heyihuang826@gmail.com>
2025-06-08 14:58:27 +0800

    fix(驗證session): 🐛 更改api端點與驗證細節修正
    * api URL從原本的更改為進階查詢的，修改取得驗證碼圖片URL、crash site URL、送出驗證碼URL
    * cookies改由session管理，取消在headers中手動覆寫

--- src/ncku_course/authenticate.py
+++ src/ncku_course/authenticate.py
@@ -19,7 +19,8 @@ def get_captcha_code(data) -> str:
     return pred_label
 
 def crash_session(session):
-    url = 'https://course.ncku.edu.tw/index.php?c=qry11215&m=en_query'
+    # url = 'https://course.ncku.edu.tw/index.php?c=qry11215&m=en_query'
+    url = 'https://course-query.acad.ncku.edu.tw/index.php?c=qry11215&m=en_query'
     for i in range(50): 
         res = session.post(url)
         if id_ := get_code_ticket_id(str(res.text)):
@@ -35,32 +36,33 @@ def get_code_ticket_id(text):
     return False
 
 def verify_session(session, code_ticket, code):
-    url = 'https://course.ncku.edu.tw/index.php?c=portal&m=robot'
+    url = 'https://course-query.acad.ncku.edu.tw/index.php?c=portal&m=robot'
     time_num = int(time.time())
     payload = {'time': f'{time_num}', 
                'code_ticket': f'{code_ticket}', 
                 'code': f'{code}'}
-    session_cookies = session.cookies.get_dict()
+    # session_cookies = session.cookies.get_dict()
     headers = {
          'Accept': '*/*',
-         'Content-Length': '74', 
-         'Accept-Encoding': 'gzip, deflate, br, zstd',
+        #  'Content-Length': '74', 
+        #  'Accept-Encoding': 'gzip, deflate, br, zstd',
          'Accept-Language': 'zh-TW,zh;q=0.9,en;q=0.8,en-US;q=0.7,zh-CN;q=0.6',
-         'Cache-Control': 'no-cache',
-         'Connection': 'keep-alive',
-         'Host': 'course.ncku.edu.tw',
+        'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        #  'Cache-Control': 'no-cache',
+        #  'Connection': 'keep-alive',
+         'Host': 'course-query.acad.ncku.edu.tw',
          'X-Requested-With': 'XMLHttpRequest', 
-         'Pragma': 'no-cache',
-         'referer': 'https://course.ncku.edu.tw/index.php?c=qry11215&m=en_query', 
-         'Sec-Ch-Ua': '"Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"',
-         'Sec-Ch-Ua-Mobile': '?0',
-         'Sec-Ch-Ua-Platform': '"Windows"',
-         'Sec-Fetch-Dest': 'empty',
-         'Sec-Fetch-Mode': 'cors',
-         'Sec-Fetch-Site': 'same-origin',
-         'Sec-Fetch-User': '?1',
+        #  'Pragma': 'no-cache',
+         'referer': 'https://course-query.acad.ncku.edu.tw/index.php?c=qry11215&m=en_query',
+        #  'Sec-Ch-Ua': '"Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"',
+        #  'Sec-Ch-Ua-Mobile': '?0',
+        #  'Sec-Ch-Ua-Platform': '"Windows"',
+        #  'Sec-Fetch-Dest': 'empty',
+        #  'Sec-Fetch-Mode': 'cors',
+        #  'Sec-Fetch-Site': 'same-origin',
+        #  'Sec-Fetch-User': '?1',
          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36', 
-         'Cookie': f'PHPSESSID={session_cookies["PHPSESSID"]}; COURSE_WEB={session_cookies["COURSE_WEB"]}'
+        #  'Cookie': f'PHPSESSID={session_cookies["PHPSESSID"]};cos_lang=cht'
          }
     
     res = session.post(url, headers=headers, data = payload, timeout=10)
@@ -71,7 +73,7 @@ def authenticate(try_limit : int = 10):
     session = requests.Session()
     _, code = crash_session(session)
     for _ in range(try_limit):
-        res = session.get(f'https://course.ncku.edu.tw/index.php?c=portal&m=robot&{int(time.time())}')
+        res = session.get(f'https://course-query.acad.ncku.edu.tw/index.php?c=portal&m=robot&{int(time.time())}')
         content = res.content
         label = get_captcha_code(content)
         success = verify_session(session, code, label.upper())
